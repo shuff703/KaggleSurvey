50fbc0e83bcb6daa368ac3b5d895cab4359c0424
Scott Huff <shuff703@gmail.com>
2018-04-23 19:27:55 -0400

    Added visualization functions

--- KaggleSurvey.R
+++ KaggleSurvey.R
@@ -3,6 +3,10 @@ setwd('Documents/R/KaggleSurvey')
 kaggle_data <- read.csv('multipleChoiceResponses.csv')
 
 #Filter out Data Scientists
+
+#WHOLESETT
+whole_set <- kaggle_data
+
 data_science <- kaggle_data[kaggle_data$CurrentJobTitleSelect == 'Data Scientist',]
 #Filter Data by Country
 usa <- kaggle_data[kaggle_data$Country == 'United States',]
@@ -26,6 +30,7 @@ print(nrow(germany_male))
 print(nrow(germany_fem))
 print(nrow(india_male))
 print(nrow(india_fem))
+print(nrow(whole_set))
 
 
 #Clean the Data
@@ -39,7 +44,7 @@ clean <- function(df, includeJob = TRUE){
   else df <- select(df, 'LanguageRecommendationSelect', 'LearningPlatformSelect', 'CoursePlatformSelect', 'MLSkillsSelect', 'MLTechniquesSelect', 'WorkAlgorithmsSelect', 'WorkToolsSelect', 'WorkMethodsSelect')
   flat_data <- cSplit(df, c('LearningPlatformSelect', 'CoursePlatformSelect', 'MLSkillsSelect', 'MLTechniquesSelect', 'WorkAlgorithmsSelect', 'WorkToolsSelect', 'WorkMethodsSelect'), sep = ',')
   flat_data$ID <- seq.int(nrow(flat_data))
-  print(flat_data)
+  # uncomment this to see printed format of data print(flat_data)
   transactions <- gather(flat_data, key, Item , -ID, na.rm = TRUE) %>%
     filter(Item != '') %>%
     select(-key) %>%
@@ -58,33 +63,32 @@ clean_germany_f <- clean(germany_fem)
 
 clean_data_science <- clean(data_science, FALSE)
 
+clean_whole_set <- clean(whole_set)
+
 #TEST THE RULES FELLERS
 #Function to avoid repetition
 getRules <- function (df, supp, conf){
   #Write the transactions to .csv
   # Create a temporary directory
-  dir.create(path = "tmp", showWarnings = FALSE)
+  dir.create(path = 'tmp', showWarnings = FALSE)
   
   # Write our data.frame to a csv
-  write.csv(df, "./tmp/transactions.csv")
+  write.csv(df, './tmp/transactions.csv')
   #Run Association Rules
-  if(!require(arules)) install.packages("arules")
+  if(!require(arules)) install.packages('arules')
   library(arules)
-  #### Large dataset ####
   
-  # Reading the data set as a dataframe object creates many problems.
-  # So, let us read the data set as a transaction object.
-  transactions<-read.transactions("tmp/transactions.csv", format="single", cols = c('ID', 'Item'), sep=",", rm.duplicates = T)
+  transactions <- read.transactions('tmp/transactions.csv', format = 'single', cols = c('ID', 'Item'), sep = ',', rm.duplicates = T)
   
   # Frequent itemsets
-  frqisets<-apriori(transactions, parameter=list(minlen=2, supp=supp, conf=conf, target="frequent itemsets"))
-  inspect(sort(frqisets, by="support"))
+  frqisets <- apriori(transactions, parameter=list(minlen = 2, supp = supp, conf = conf, target = 'frequent itemsets'))
+  inspect(sort(frqisets, by = 'support'))
   
   # Association rules
-  rules<-apriori(transactions, parameter=list(minlen=2, supp=supp, conf=conf, target="rules"))
+  rules<-apriori(transactions, parameter = list(minlen = 2, supp = supp, conf = conf, target = 'rules'))
   #inspect(rules)
-  rules.sorted<-sort(rules, by="lift")
-  inspect(rules.sorted)
+  rules.sorted<-sort(rules, by = 'lift')
+  inspect(rules.sorted[1:50])
   
   #quality(rules)<-round(quality(rules), digits=3)
   #inspect(sort(rules,by="confidence"))
@@ -98,23 +102,50 @@ getRules(clean_germany_m, 0.2, .95)
 getRules(clean_germany_f, 0.2, .95)
 getRules(clean_india_m, 0.1, .95)
 getRules(clean_india_f, 0.1, .95)
-getRules(clean_data_science, 0.5, .95)
-#TODO: Implement Visualizations as functions
+getRules(clean_data_science, 0.1, .95)
+
+rules <- getRules(clean_whole_set, .05, .9)
 
 #DONT RUN THESE YET IT MAY BLOW YOUR ENV UP
+baseGraph <- function (rules){
+  if(!require(arulesViz)) install.packages("arulesViz")
+  library(arulesViz)
+  rules.sorted <- sort(rules, by='lift')
+  plot(rules.sorted)
+}
 
-if(!require(arulesViz)) install.packages("arulesViz")
-library(arulesViz)
+groupedGraph <- function (rules){
+  if(!require(arulesViz)) install.packages("arulesViz")
+  library(arulesViz)
+  rules.sorted <- sort(rules, by='lift')
+  plot(rules.sorted, method="grouped")
+}
+
+itemGraph <- function (rules){
+  if(!require(arulesViz)) install.packages("arulesViz")
+  library(arulesViz)
+  rules.sorted <- sort(rules, by='lift')
+  plot(rules.sorted, method="graph", control=list(type="items"))
+}
+
+paracordGraph <- function (rules){
+  if(!require(arulesViz)) install.packages("arulesViz")
+  library(arulesViz)
+  rules.sorted <- sort(rules, by='lift')
+  plot(rules.sorted, method="paracoord")
+}
+
+plotSingleRule(rule, rules){
+  if(!require(arulesViz)) install.packages("arulesViz")
+  library(arulesViz)
+  rules.sorted <- sort(rules, by='lift')
+  set.seed(6201)
+  onerule<-sample(rules.sorted, 1)
+  inspect(onerule)
+  plot(onerule, method="doubledecker", data=rules)
+}
 # Different plots
-plot(rules.sorted)
-plot(rules.sorted, method="grouped")
-plot(rules.sorted, method="graph", control=list(type="items"))
-plot(rules.sorted, method="paracoord")
-# Deep dive into a single rule
-set.seed(6201)
-onerule<-sample(rules.sorted, 1)
-inspect(onerule)
-plot(onerule, method="doubledecker", data=transactions)
+baseGraph(rules)
 
 
 
